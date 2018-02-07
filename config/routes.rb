acd78c8315fcf8737b4216af572ab291c6e4e77f
Jayanth Dungavath <jayanthdeejay@gmail.com>
2018-02-07 14:31:55 -0500

    Roll back to Hello World to start fresh for the sample app

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  resources :microposts
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'controller_name#action_name'
-  #root 'application#hello'
-  root 'users#index'
+  root 'application#hello'
 end
